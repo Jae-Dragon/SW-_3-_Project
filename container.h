6d73bae247166333fd23db7e31f25052937317e7
JB <jaeyongjeong818@gmail.com>
2022-10-31 13:55:37 +0900

    초기 커밋.

---
+++ container.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <windows.h>
+void SetCurrentCursorPos(int x, int y)
+{
+	COORD position = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
+COORD GetCurrentCursorPos(void)
+{
+	COORD curPoint;
+	CONSOLE_SCREEN_BUFFER_INFO curInfo;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curPoint.X = curInfo.dwCursorPosition.X;
+	curPoint.Y = curInfo.dwCursorPosition.Y;
+
+	return curPoint;
+}
+void RemoveCursor(void)
+{
+	CONSOLE_CURSOR_INFO curInfo;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curInfo.bVisible = 0;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+}
+void textcolor(int color_number)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_number);
+}
---
+++ domain.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+#include <limits.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include "container.h"
+
+void Title();
+
+int main()
+{
+	RemoveCursor();
+	system("mode con cols=130 lines=34");
+	//cols = 칸/행 (가로)  lines = 줄/열 (세로)
+
+	Title();
+
+	return 0;
+}
+
+void Title()
+{
+	COORD curPos;
+	//curPos = GetCurrentCursorPos();
+	curPos.X = 60; curPos.Y = 15;
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+	textcolor(14);
+	printf("내일은 주식왕!!!\n");
+	getchar();
+	textcolor(15);
+}
+
