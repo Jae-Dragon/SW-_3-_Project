6e0544c52eca11d77c978ee7db83baa8c4bad8b6
JB <102784566+MiniYongee@users.noreply.github.com>
2022-11-04 19:26:58 +0900

    2022.11.04(DrawingMarket까지)

--- domain.c
+++ domain.c
@@ -10,13 +10,23 @@
 #include "define.h"
 #include "visible.h"
 
+#define Market_WIDTH 35
+#define Market_HEIGHT 20
+#define Market_ORIGIN_X 25
+#define Market_ORIGIN_Y 7
+
+
 char EventList[30][30];
-char StockList[5][30];
+char StockList[30][30];
 
 void Title();
 
 void Stock();
 void Event();
+int Random(int min, int max);
+void Opinion(int* Choice, char * YorN);
+void Drawingmarket();
+void EraseMarket();
 
 int main()
 {
@@ -24,27 +34,38 @@ int main()
 	system("mode con cols=130 lines=34");
 	//cols = 칸/행 (가로)  lines = 줄/열 (세로)
 
+	/*
 	Title();
 	Event();
+	Stock();
+	int a;
+	char YORN;
+	Opinion(&a, &YORN);
+	*/
+	Drawingmarket();
+
 
 	return 0;
 }
 
 void Stock()
 {
-	strcpy(StockList[0], "제약");
-	strcpy(StockList[1], "방산");
+	//정수 입력받고 그 선택의 번호 반환
+
+	strcpy(StockList[0], "제약");//0, 1
+	strcpy(StockList[1], "방산");//2, 3
 	strcpy(StockList[2], "반도체");
 	strcpy(StockList[3], "조선");
 	strcpy(StockList[4], "제철");
+
 }
 
 void Event()
 {
 	//짝수번째 --> 주가하락 사유,  홀수번째 --> 주가상승 사유
 
-	strcpy(EventList[0], "백신 부작용 발생전염병");//제약회사 주가 하락
-	strcpy(EventList[1], "발생");//제약회사 주가 상승
+	strcpy(EventList[0], "백신 부작용 발생");//제약회사 주가 하락
+	strcpy(EventList[1], "전염병 발생");//제약회사 주가 상승
 
 	strcpy(EventList[2], "방산업체에서 비리 발생");//방산 관련 주가 하락
 	strcpy(EventList[3], "북한이 동해안으로 미사일 발사");//방상 관련 주가 상승
@@ -62,12 +83,114 @@ void Event()
 	strcpy(EventList[14], "유가가 상승하였다");//주가하락 -->경상수지가 악화되고 기업실적이 악화(원자재값과 관련)
 	strcpy(EventList[15], "유가가 하락하였다");//주가상승 -->경상수지가 호전되고 기업실적이 호전(원자재값과 관련)
 
+	int random;
 
-	gotoxy(5, 3);
-	printf("%s\n", EventList[0]);
-	gotoxy(5, 4);
-	printf("%s\n", EventList[1]);
-	gotoxy(5, 5);
-	printf("%s\n", EventList[2]);
+	for (int i = 0; i < 10; i++)
+	{
+		random = Random(0, 15);
+		gotoxy(i, i);
+		printf("%s\n", EventList[random]);
+
+		Sleep(1000);
+	}
+
 }
 
+int Random(int min, int max)
+{
+	srand(time(NULL));
+	int random = 0;
+	random = rand() % (max - min) + min;
+
+	return random;
+}
+
+void Opinion(int* Choice, char* YorN)
+{
+	textcolor(13);
+	gotoxy(7, 6);
+	printf("투자상품 목록");
+
+	textcolor(15);
+	gotoxy(7, 8);
+	printf("1.%s, 2.%s, 3.%s, 4.%s, 5.%s, 6.없음", StockList[0], StockList[1], StockList[2], StockList[3], StockList[4]);
+
+	gotoxy(7, 10);
+	printf("상품에 투자하고 싶으시면 Y를, 아니라면 N을 입력해주세요: ");
+	scanf("%c", YorN);
+
+
+	gotoxy(7, 12);
+	printf("투자하고 싶은 상품의 번호를 입력하세요: ");
+	scanf("%d", Choice);
+	
+	getchar();
+}
+
+void Drawingmarket()
+{
+	int x, y;
+
+	for (x = 2; x <= 2 * Market_WIDTH + 16; x++) {
+		SetCurrentCursorPos(Market_ORIGIN_X + x - 8, Market_ORIGIN_Y);
+
+		printf("─");
+	}
+	x = 0; y = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		SetCurrentCursorPos(Market_ORIGIN_X + x - 6, Market_ORIGIN_Y - y);
+		printf("\\");
+		x++; y++;
+	}
+	x = 0; y = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		SetCurrentCursorPos(Market_ORIGIN_X + x + Market_WIDTH * 2 + 4, Market_ORIGIN_Y + y - 6);
+		printf("\\");
+		x++; y++;
+	}
+	for (x = 2; x <= 2 * Market_WIDTH + 6; x++) {
+		SetCurrentCursorPos(Market_ORIGIN_X + x - 2, Market_ORIGIN_Y - 6);
+
+		printf("\\");
+	}
+	//===============================
+
+	for (y = 0; y <= Market_HEIGHT; y++) {
+		SetCurrentCursorPos(Market_ORIGIN_X, Market_ORIGIN_Y + y);
+
+		if (y == Market_HEIGHT) {
+			printf("┖");//└ ┗ ┗
+		}
+
+		else {
+			printf("┃");
+		}
+	}
+
+
+	for (x = 2; x <= 2 * Market_WIDTH; x++) {
+		SetCurrentCursorPos(Market_ORIGIN_X + x, Market_ORIGIN_Y + Market_HEIGHT);
+
+		printf("─");
+	}
+
+	for (y = 0; y <= Market_HEIGHT; y++) {
+		SetCurrentCursorPos(Market_ORIGIN_X + 2 * Market_WIDTH + 2, Market_ORIGIN_Y + y);//
+
+		if (y == Market_HEIGHT) {
+			printf("┚");//┘┚
+		}
+
+		else {
+			printf("┃");
+		}
+	}
+
+
+}
+
+void EraseMarket()
+{
+}
