12a047bd9fb49e05f8d818124adcfe7bae0a7464
JB <102784566+MiniYongee@users.noreply.github.com>
2022-11-09 15:10:54 +0900

    2022.11.09(개미 외형 변경, 점프추가)

--- visible.h
+++ visible.h
@@ -29,44 +29,94 @@ void Background()
     for (int i = 0; i < 15; i++)
         printf("────────");
 }
+// 키보드 입력된 값을 반환하는 함수
+int GetKeyDown()
+{
+    if (_kbhit() != 0)
+    {
+        return _getch();
+    }
+    return 0;
+}
 
 void Ant()
 {
+
+
+    int isJumping = 0;
+    int isBottom = 1;
+    const int gravity = 3;
+
+    int antY = 26;
+
+
     int leg = 0;
+    Background();
+    //int idx = 1;
     while (1)
     {
-        Background();
-        gotoxy(0, 20);
 
-        printf("            ■  ■  \n");
-        printf("            ■  ■  \n");
-        printf("        ■■■■■  \n");
-        printf("        ■■■  ■  \n");
-        printf("        ■■■■■  \n");
-        printf("        ■■■■■  \n");
-        printf("          ■■■    \n");
-        printf("      ■■■■■■■\n");
-        printf("  ■■■■■■■  ■\n");
-        printf("■■■■■■■■■  \n");
-        printf("■■■■■■■  ■  \n");
-        printf("■■■■■■        \n");
-        printf("  ■■■■          \n");
+        // z키를 누르고, 바닥일 때만 점프가능
+        if (GetKeyDown() == ' ' && isBottom)
+        {
+            isJumping = 1;
+            isBottom = 0;
+        }
+
+        if (isJumping)
+        {
+            antY -= gravity;
+        }
+        else
+        {
+            antY += gravity;
+        }
+
+        if (antY >= 26)
+        {
+            antY = 26;
+            isBottom = 1;
+        }
+
+        if (antY <= 3)
+        {
+            isJumping = 0;
+        }
+
+        Background();
+        gotoxy(0, antY);
+
+        //textcolor(idx++);
+        //if (idx > 15) idx = 1;
+        printf("      ■■  \n");
+        printf("      ■    \n");
+        printf("    ■■■■\n");
+        printf("    ■■  ■\n");
+        printf("    ■■■■\n");
+        printf("      ■■  \n");
+        printf("■■■■■  \n");
+        printf("■■■■  \n");
         if (leg == 0)
         {
-            printf("  ■    ■■■      \n");
-            printf("  ■■              \n");
+            printf(" /＼ /＼\n");
             leg = 1;
         }
         else
         {
-            printf("  ■■  ■          \n");
-            printf("        ■■        \n");
+            printf(" ＼/ ＼/\n");
             leg = 0;
         }
 
-        Sleep(100);
+        gotoxy(40, 20);
+        printf("********\n");
+        gotoxy(40, 21);
+        printf("*  100 *\n");
+        gotoxy(40, 22);
+        printf("********\n");
+
+        Sleep(10);
         system("cls");
     }
 
-
 }
+
